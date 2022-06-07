7329fe210cda2cd64c30830918296c6237415f02
thaian161 <thaian161@yahoo.com>
2022-06-06 20:13:35 -0500

    refactor middle and middleTest

--- middle.js
+++ middle.js
@@ -1,3 +1,7 @@
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+
 /*The console.log(middle function should return an array with only the console.log(middle element(s) of the provided array.
   Arrays with one or two elements, there is no console.log(middle 
     => Return an empty array
@@ -8,28 +12,6 @@
   Arrays with an even number of elements 
     => Return 2 element in the console.log(middle
 */
-
-// eqArays function
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-//assertArraysEqual function
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`✔️✔️✔️: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌❌❌: ${arr1} !== ${arr2}`);
-  }
-};
 
 //middle function
 const middle = function (arr) {
@@ -49,10 +31,4 @@ const middle = function (arr) {
   return result;
 };
 
-// ===TEST CODE===
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+// ===TEST CODE===
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
